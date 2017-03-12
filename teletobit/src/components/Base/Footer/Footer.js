69dbb803330d846918ff766c1d4ed789e2f3bfd7
EDEN PARK <edenp@techday.com>
2017-03-13 08:46:39 +1300

    update footer text

--- teletobit/src/components/Base/Footer/Footer.js
+++ teletobit/src/components/Base/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className="footer">
-          Made by <a href="mailto:edenpark.dev@gmail.com"><b>Eden Park</b></a>
+		© 2017 Copyright by <a href="mailto:edenpark.dev@gmail.com"><b>Eden Park</b></a>. All rights reserved.
         </div>
     );
 };
